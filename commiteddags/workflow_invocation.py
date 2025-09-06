ea4ec92b46074668229dad19480eaafa7c189b71
Alp-i <alperenbaydur00@gmail.com>
2025-09-06 23:21:25 +0300

    .

---
+++ commiteddags/workflow_invocation.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+from google.cloud.dataform_v1beta1 import WorkflowInvocation
+
+from airflow import models
+from airflow.models.baseoperator import chain
+from airflow.providers.google.cloud.operators.dataform import (
+    DataformCreateCompilationResultOperator,
+    DataformCreateWorkflowInvocationOperator,
+)
+from airflow.providers.google.cloud.sensors.dataform import DataformWorkflowInvocationStateSensor
+
+
+DAG_ID = "dataform"
+PROJECT_ID = "datapipeline-468807"
+REPOSITORY_ID = "rep"
+REGION = "us-east1"
+GIT_COMMITISH = "dataform-workspace"
+
+
+with models.DAG(
+    DAG_ID,
+    schedule_interval='@once',  # Override to match your needs
+    start_date=datetime(2022, 1, 1),
+    catchup=False,  # Override to match your needs
+    tags=['dataform'],
+) as dag:
+
+    create_compilation_result = DataformCreateCompilationResultOperator(
+        task_id="create_compilation_result",
+        project_id=PROJECT_ID,
+        region=REGION,
+        repository_id=REPOSITORY_ID,
+        compilation_result={
+            "git_commitish": GIT_COMMITISH,
+        },
+    )
+
+create_workflow_invocation = DataformCreateWorkflowInvocationOperator(
+    task_id='create_workflow_invocation',
+    project_id=PROJECT_ID,
+    region=REGION,
+    repository_id=REPOSITORY_ID,
+    asynchronous=True,
+    workflow_invocation={
+        "compilation_result": "{{ task_instance.xcom_pull('create_compilation_result')['name'] }}"
+    }
+)
+
+is_workflow_invocation_done = DataformWorkflowInvocationStateSensor(
+    task_id="is_workflow_invocation_done",
+    project_id=PROJECT_ID,
+    region=REGION,
+    repository_id=REPOSITORY_ID,
+    workflow_invocation_id=("{{ task_instance.xcom_pull('create_workflow_invocation')['name'].split('/')[-1] }}"),
+    expected_statuses={WorkflowInvocation.State.SUCCEEDED},
+)
+
+
+create_compilation_result >> create_workflow_invocation
