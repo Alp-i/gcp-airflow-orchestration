891cb6201c5924cd3f16986360fbad98129f0b6b
Alp-i <alperenbaydur00@gmail.com>
2025-09-09 16:40:47 +0300

    .

--- commiteddags/dataflow_events_dag.py
+++ commiteddags/dataflow_events_dag.py
@@ -1,61 +1,100 @@
 from datetime import datetime
 from airflow import models
 from airflow.providers.google.cloud.operators.dataflow import DataflowStartFlexTemplateOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
 from airflow.models import Variable
 
+# Define Airflow Variables for connections and settings
 db_ip = Variable.get("mysql_ip")
 connection_url = f"jdbc:mysql://{db_ip}:3306/clothing_db"
 
 # Constants
 PROJECT_ID = "datapipeline-468807"
 LOCATION = "us-east1"
-BODY = {
-    "launch_parameter": {
-        "jobName": "df-customers-table",
-        "containerSpecGcsPath": "gs://dataflow-templates-us-east1/latest/flex/MySQL_to_BigQuery",
-        "parameters": {
-            "connectionURL": connection_url,
-            "username": "root",
-            "password": "54092021Aa!",
-            "query": "SELECT * FROM sales_events",
-            "outputTable": "datapipeline-468807:landingzone.clothing_db_customers_copy",
-            "bigQueryLoadingTemporaryDirectory": "gs://lcw-dataflow-temp-bucket",
-            "useColumnAlias": "false",
-            "isTruncate": "false",
-            "partitionColumnType": "long",
-            "fetchSize": "50000",
-            "createDisposition": "CREATE_NEVER",
-            "useStorageWriteApi": "false",
-            "stagingLocation": "gs://dataflow-staging-us-east1-377358662798/staging",
-            "autoscalingAlgorithm": "NONE",
-            "serviceAccount": "377358662798-compute@developer.gserviceaccount.com",
-            "labels": "{\"goog-dataflow-provided-template-name\":\"mysql_to_bigquery\",\"goog-dataflow-provided-template-type\":\"flex\",\"goog-dataflow-provided-template-version\":\"2025-08-26-00_rc00\"}"
-        },
-        "environment": {
-            "numWorkers": 2,
-            "tempLocation": "gs://dataflow-staging-us-east1-377358662798/tmp",
-            "additionalExperiments": [
-                "use_runner_v2"
-            ],
-            "additionalUserLabels": {}
-        }
-    }
-}
+DATASET_ID = "landingzone"
+SOURCE_TABLE = "sales_events"
+WATERMARK_TABLE = f"{PROJECT_ID}:{DATASET_ID}.watermark_table"
+LANDING_ZONE_TABLE = f"{PROJECT_ID}:{DATASET_ID}.clothing_db_sales_events"
 
 # DAG definition
 with models.DAG(
-    "dataflow_events_dag",
+    "dataflow_events_incremental_dag",
     schedule_interval='@once',
     start_date=datetime(2025, 9, 6),
     catchup=False,
-    tags=['dataflow'],
+    tags=['dataflow', 'incremental'],
 ) as dag:
 
+    # Task 1: Get the last processed watermark from BigQuery
+    # The result is pushed to XCom for the next task.
+    get_watermark_task = BigQueryExecuteQueryOperator(
+        task_id='get_watermark_task',
+        sql=f"SELECT last_processed_timestamp FROM `{WATERMARK_TABLE}` WHERE table_name = '{SOURCE_TABLE}'",
+        use_legacy_sql=False,
+        gcp_conn_id='google_cloud_default',
+        do_xcom_push=True,
+    )
+
+    # Task 2: Start the Dataflow job with the dynamic query
+    # The query is built dynamically to filter data.
+    # The `{{ ti.xcom_pull(...) }}` is Jinja templating to get the watermark from the previous task.
     start_flex_template_job = DataflowStartFlexTemplateOperator(
         task_id="start_flex_template_job",
         project_id=PROJECT_ID,
-        body=BODY,
+        body={
+            "launch_parameter": {
+                "jobName": f"df-events-table-{datetime.now().strftime('%Y%m%d%H%M%S')}",
+                "containerSpecGcsPath": "gs://dataflow-templates-us-east1/latest/flex/MySQL_to_BigQuery",
+                "parameters": {
+                    "connectionURL": connection_url,
+                    "username": "root",
+                    "password": "54092021Aa!",
+                    "query": f"SELECT * FROM {SOURCE_TABLE} WHERE timestamp > '{{{{ ti.xcom_pull(task_ids='get_watermark_task', key='return_value')[0][0] }}}}'",
+                    "outputTable": LANDING_ZONE_TABLE,
+                    "bigQueryLoadingTemporaryDirectory": "gs://lcw-dataflow-temp-bucket",
+                    "useColumnAlias": "false",
+                    "isTruncate": "false",
+                    "partitionColumnType": "long",
+                    "fetchSize": "50000",
+                    "createDisposition": "CREATE_NEVER",
+                    "useStorageWriteApi": "false",
+                    "stagingLocation": "gs://dataflow-staging-us-east1-377358662798/staging",
+                    "autoscalingAlgorithm": "NONE",
+                    "serviceAccount": "377358662798-compute@developer.gserviceaccount.com"
+                },
+                "environment": {
+                    "numWorkers": 2,
+                    "tempLocation": "gs://dataflow-staging-us-east1-377358662798/tmp",
+                    "additionalExperiments": ["use_runner_v2"],
+                    "additionalUserLabels": {}
+                }
+            }
+        },
         location=LOCATION,
         append_job_name=False,
-        wait_until_finished=True,  # Non-deferrable: DAG waits until job finishes
+        wait_until_finished=True,
     )
+
+    # Task 3: Update the watermark in BigQuery
+    update_watermark_task = BigQueryExecuteQueryOperator(
+        task_id="update_watermark",
+        sql=f"""
+            MERGE `{WATERMARK_TABLE}` AS T
+            USING (
+              SELECT '{SOURCE_TABLE}' AS table_name, MAX(timestamp) AS new_watermark
+              FROM `{LANDING_ZONE_TABLE}`
+            ) AS S
+            ON T.table_name = S.table_name
+
+            WHEN MATCHED THEN
+              UPDATE SET last_processed_timestamp = S.new_watermark
+
+            WHEN NOT MATCHED THEN
+              INSERT (table_name, last_processed_timestamp) VALUES(S.table_name, S.new_watermark);
+        """,
+        use_legacy_sql=False,
+        gcp_conn_id='google_cloud_default',
+    )
+
+    # Define the task dependencies
+    get_watermark_task >> start_flex_template_job >> update_watermark_task
